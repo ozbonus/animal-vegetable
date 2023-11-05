6ef62f6e5a3db6bef14ed37ed11a5bfb8066cacf
Christopher Perry <ozbonus@gmail.com>
2023-11-06 01:18:33 +0800

    Create player banners for attract screen.

---
+++ player_banner.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+@export var banner: Texture2D
+@export var player_number: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$BannerSprite.texture = banner
+	$PlayerNumber.text = player_number
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
