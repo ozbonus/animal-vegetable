55f2dc53def0a2a9eeabf2df1ec4897bb06e6980
Christopher Perry <ozbonus@gmail.com>
2023-11-07 17:22:54 +0800

    Unnecessary _process function.

--- attract_screen.gd
+++ attract_screen.gd
@@ -9,11 +9,6 @@ func _ready():
 	$P3BannerAnimation.play("RESET")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_p_1_toggle_active_button_pressed():
 	if !ActivePlayersRepository.p1_active:
 		$P1BannerAnimation.play("slide")
