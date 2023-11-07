d04575079e4198a42c6d77a02b7ce302403f4ff8
Christopher Perry <ozbonus@gmail.com>
2023-11-07 17:21:09 +0800

    Reset players, score, animations on load.

--- attract_screen.gd
+++ attract_screen.gd
@@ -1,9 +1,12 @@
 extends CanvasLayer
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	ActivePlayersRepository.reset()
+	ScoreRepository.reset()
+	$P1BannerAnimation.play("RESET")
+	$P2BannerAnimation.play("RESET")
+	$P3BannerAnimation.play("RESET")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
