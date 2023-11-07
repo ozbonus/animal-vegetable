b80bb740b47afa442a3a59737a2ea83fb6483e4a
Christopher Perry <ozbonus@gmail.com>
2023-11-07 16:02:00 +0800

    Remove dead code.

--- score_repository.gd
+++ score_repository.gd
@@ -4,15 +4,6 @@ var p1_score: int = 0
 var p2_score: int = 0
 var p3_score: int = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func add_p1_score(points: int):
 	p1_score += points
 	
