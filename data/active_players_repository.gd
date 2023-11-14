fa0f22d1b61dc63cb5c2d1ea40cfce07548d05a4
Christopher Perry <ozbonus@gmail.com>
2023-11-15 01:35:25 +0800

    CHORE: Refactor title screen. (#6)
    Much smaller scripts and more efficient scenes for the banners.

--- data/active_players_repository.gd
+++ data/active_players_repository.gd
@@ -1,33 +1,35 @@
 extends Node
 
+
 var p1_active: bool = false
 var p2_active: bool = false
 var p3_active: bool = false
-var active_msg = "Player {p} active: {b}"
+var p4_active: bool = false
 
-func p1_toggle_active():
+
+func p1_toggle_active() -> void:
 	p1_active = !p1_active
-	print(active_msg.format({
-		"p": "1",
-		"b": str(p1_active),
-	}))
+	print("Player 1 active: ", p1_active)
 
-func p2_toggle_active():
+
+func p2_toggle_active() -> void:
 	p2_active = !p2_active
-	print(active_msg.format({
-		"p": "2",
-		"b": str(p2_active),
-	}))
+	print("Player 2 active: ", p2_active)
 
-func p3_toggle_active():
+
+func p3_toggle_active() -> void:
 	p3_active = !p3_active
-	print(active_msg.format({
-		"p": "3",
-		"b": str(p3_active),
-	}))
+	print("Player 3 active: ", p3_active)
+
+
+func p4_toggle_active() -> void:
+	p4_active = !p4_active
+	print("Player 4 active: ", p4_active)
+
 
 func reset():
 	p1_active = false
 	p2_active = false
 	p3_active = false
+	p4_active = false
 	print("RESET: All players are inactive.")
--- title_screen.gd
+++ title_screen.gd
@@ -4,46 +4,50 @@ extends Node
 func _ready():
 	ActivePlayersRepository.reset()
 	PlayerRepository.reset()
-	$P1BannerAnimation.play("RESET")
-	$P2BannerAnimation.play("RESET")
-	$P3BannerAnimation.play("RESET")
+	$BannerAnimation.play("RESET")
+	
 
 func _process(delta):
 	if Input.is_action_pressed("next_screen"):
 		ScreenTransition.change_screen("res://age_screen.tscn")
-	elif Input.is_action_just_pressed("p1_toggle_active"):
+	if Input.is_action_just_pressed("command_1"):
 		p1_toggle_active()
-	elif Input.is_action_just_pressed("p2_toggle_active"):
+	if Input.is_action_just_pressed("command_2"):
 		p2_toggle_active()
-	elif Input.is_action_just_pressed("p3_toggle_active"):
+	if Input.is_action_just_pressed("command_3"):
 		p3_toggle_active()
+	if Input.is_action_just_pressed("command_4"):
+		p4_toggle_active()
+	
 
 
 func p1_toggle_active():
 	if !ActivePlayersRepository.p1_active:
-		$P1BannerAnimation.play("slide")
-		$PlayerBanner1.play_activate_sound()
+		$PlayerReadyBanner1.activate()
 	else:
-		$P1BannerAnimation.play_backwards("slide")
-		$PlayerBanner1.play_deactivate_sound()
+		$PlayerReadyBanner1.deactivate()
 	ActivePlayersRepository.p1_toggle_active()
 
 
 func p2_toggle_active():
 	if !ActivePlayersRepository.p2_active:
-		$P2BannerAnimation.play("slide")
-		$PlayerBanner2.play_activate_sound()
+		$PlayerReadyBanner2.activate()
 	else:
-		$P2BannerAnimation.play_backwards("slide")
-		$PlayerBanner2.play_deactivate_sound()
+		$PlayerReadyBanner2.deactivate()
 	ActivePlayersRepository.p2_toggle_active()
 
 
 func p3_toggle_active():
 	if !ActivePlayersRepository.p3_active:
-		$P3BannerAnimation.play("slide")
-		$PlayerBanner3.play_activate_sound()
+		$PlayerReadyBanner3.activate()
 	else:
-		$P3BannerAnimation.play_backwards("slide")
-		$PlayerBanner3.play_deactivate_sound()
+		$PlayerReadyBanner3.deactivate()
 	ActivePlayersRepository.p3_toggle_active()
+
+
+func p4_toggle_active():
+	if !ActivePlayersRepository.p4_active:
+		$PlayerReadyBanner4.activate()
+	else:
+		$PlayerReadyBanner4.deactivate()
+	ActivePlayersRepository.p4_toggle_active()
