404ac2639914b5ff371661a3498788d98d8ca653
Christopher Perry <ozbonus@gmail.com>
2023-11-18 23:11:02 +0800

    Set up tutorial screen.

--- tutorial_screen.gd
+++ tutorial_screen.gd
@@ -1,7 +1,36 @@
 extends Node
 
+var cards_1: Array[Node] = [$P1Area/Card1, $P2Area/Card1, $P2Area/Card1, $P2Area/Card1]
+var cards_2: Array[Node] = [$P1Area/Card2, $P2Area/Card2, $P2Area/Card2, $P2Area/Card2]
+var cards_3: Array[Node] = [$P1Area/Card3, $P2Area/Card3, $P2Area/Card3, $P2Area/Card3]
+var cards_4: Array[Node] = [$P1Area/Card4, $P2Area/Card4, $P2Area/Card4, $P2Area/Card4]
+var arrows_1: Array[Node] = [
+	$P1Area/TargetArrows/TargetArrow1,
+	$P2Area/TargetArrows/TargetArrow1,
+	$P3Area/TargetArrows/TargetArrow1,
+	$P4Area/TargetArrows/TargetArrow1,
+]
+var arrows_2: Array[Node] = [
+	$P1Area/TargetArrows/TargetArrow2,
+	$P2Area/TargetArrows/TargetArrow2,
+	$P3Area/TargetArrows/TargetArrow2,
+	$P4Area/TargetArrows/TargetArrow2,
+]
+var arrows_3: Array[Node] = [
+	$P1Area/TargetArrows/TargetArrow3,
+	$P2Area/TargetArrows/TargetArrow3,
+	$P3Area/TargetArrows/TargetArrow3,
+	$P4Area/TargetArrows/TargetArrow3,
+]
+var arrows_4: Array[Node] = [
+	$P1Area/TargetArrows/TargetArrow4,
+	$P2Area/TargetArrows/TargetArrow4,
+	$P3Area/TargetArrows/TargetArrow4,
+	$P4Area/TargetArrows/TargetArrow4,
+]
 
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready():
 	MusicService.play_title_music()
 
@@ -12,3 +41,5 @@ func _process(delta):
 		ScreenTransition.change_screen("res://play_screen.tscn")
 	if Input.is_action_just_pressed("previous_screen"):
 		ScreenTransition.change_screen("res://age_screen.tscn")
+	if Input.is_action_just_pressed("p1_left"):
+		$Card.disappear_correct()
