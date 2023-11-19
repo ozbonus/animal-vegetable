e3211b58df7cb45419d00f86b5eb02770d86b39d
Christopher Perry <ozbonus@gmail.com>
2023-11-19 22:03:01 +0800

    Place timer in scene.

--- play_screen.gd
+++ play_screen.gd
@@ -1,11 +1,16 @@
 extends Node
 
 
+enum State {COUNTDOWN, PLAY, FINISHED}
+
+
 @export var debug: bool = false
+@export var debug_state: State
 var p1_active: bool = ActivePlayersRepository.p1_active
 var p2_active: bool = ActivePlayersRepository.p2_active
 var p3_active: bool = ActivePlayersRepository.p3_active
 var p4_active: bool = ActivePlayersRepository.p4_active
+var state: State
 
 
 func _ready():
@@ -19,13 +24,24 @@ func _ready():
 		$P3PlayArea.queue_free()
 	if !p4_active and !debug:
 		$P4PlayArea.queue_free()
+	
+	if debug:
+		state = debug_state
+	else:
+		state = State.COUNTDOWN
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	if Input.is_action_just_pressed("previous_screen"):
 		ScreenTransition.change_screen("res://tutorial_screen.tscn")
 	
 	if Input.is_action_just_pressed("next_screen"):
 		ScreenTransition.change_screen("res://score_screen.tscn")
+	
+	match state:
+		State.COUNTDOWN:
+			pass
+		State.PLAY:
+			
 	
