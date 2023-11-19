f0ee3d5b0cc59e2f46e2eae90500143e881dec7f
Christopher Perry <ozbonus@gmail.com>
2023-11-19 22:36:29 +0800

    Make stop music methods public.

--- applcation/music_service.gd
+++ applcation/music_service.gd
@@ -2,13 +2,13 @@ extends Node
 
 
 func play_title_music(delay := 0.0) -> void:
-	_stop_gameplay_music()
+	stop_gameplay_music()
 	if !$TitleMusic.playing:
 		await get_tree().create_timer(delay).timeout
 		$TitleMusic.play()
 
 
-func _stop_title_music() -> void:
+func stop_title_music() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property($TitleMusic, "volume_db", -80.0, 2.0)
 	tween.connect("finished", _on_stop_title_music_tween)
@@ -20,13 +20,13 @@ func _on_stop_title_music_tween() -> void:
 
 
 func play_gameplay_music(delay := 0.0) -> void:
-	_stop_title_music()
+	stop_title_music()
 	if !$GameplayMusic.playing:
 		await get_tree().create_timer(delay).timeout
 		$GameplayMusic.play()
 
 
-func _stop_gameplay_music() -> void:
+func stop_gameplay_music() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property($GameplayMusic, "volume_db", -80.0, 2.0)
 	tween.connect("finished", _on_stop_gameplay_music_tween)
