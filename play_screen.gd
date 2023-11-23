e7a52783ed6afa0fe879f525badf1ef9c098074a
Christopher Perry <ozbonus@gmail.com>
2023-11-23 11:17:55 +0800

    FIX: Call method on activie players without errors. (#26)

--- play_screen.gd
+++ play_screen.gd
@@ -6,6 +6,7 @@ var p1_active: bool = ActivePlayersRepository.p1_active
 var p2_active: bool = ActivePlayersRepository.p2_active
 var p3_active: bool = ActivePlayersRepository.p3_active
 var p4_active: bool = ActivePlayersRepository.p4_active
+var active_players: Array[Node]
 
 
 func _ready():
@@ -14,12 +15,20 @@ func _ready():
 	PlayerRepository.reset_scores()
 	if !p1_active and !debug:
 		$P1PlayArea.queue_free()
+	else:
+		active_players.append($P1PlayArea)
 	if !p2_active and !debug:
 		$P2PlayArea.queue_free()
+	else:
+		active_players.append($P2PlayArea)
 	if !p3_active and !debug:
 		$P3PlayArea.queue_free()
+	else:
+		active_players.append($P3PlayArea)
 	if !p4_active and !debug:
 		$P4PlayArea.queue_free()
+	else:
+		active_players.append($P4PlayArea)
 
 
 func _process(delta):
@@ -32,12 +41,12 @@ func _process(delta):
 
 func _on_countdown_animation_animation_finished(anim_name):
 	$CountdownLabel.hide()
-	[$P1PlayArea, $P2PlayArea, $P3PlayArea, $P4PlayArea].map(func(x): if x: x.start_game())
+	active_players.map(func(x): x.start_game())
 	$GameTimer.start()
 
 
 func _on_game_timer_game_over():
-	[$P1PlayArea, $P2PlayArea, $P3PlayArea, $P4PlayArea].map(func(x): if x: x.finish_game())
+	active_players.map(func(x): x.finish_game())
 	$FinishedLabel.show()
 	$FinishedLabelAnimation.play("appear")
 	$Whistle.play()
